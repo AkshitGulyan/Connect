ed5b8b58a6b07cddb1af8a8611038f4ee7e1c301
AdityyaX <adityapachauri182003@gmail.com>
2023-05-26 18:11:55 +0530

    index page removed

--- Project/connect/main/views.py
+++ Project/connect/main/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponse
+def login(request):
+    return HttpResponse("hello there")
+
+# def dash(request):
+#     return HttpResponse("hello there")
+
+def dash(request):
+    return render(request,"dashboard.html")
 
 # Create your views here.
