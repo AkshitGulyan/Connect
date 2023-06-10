56edf1403341d6cf393f68f4984af81ba0e1cd4f
Akshit Gulyan <103456810+AkshitGulyan@users.noreply.github.com>
2023-06-10 19:01:29 +0530

    Update views.py

--- connect2/authentication/views.py
+++ connect2/authentication/views.py
@@ -38,7 +38,7 @@ def signup(request):
         messages.success(request,'Your account has been successfuly created')
 
         #Welcome EMAIL
-        subject = 'Welcmoe to Connect'
+        subject = 'Welcoe to Connect'
         message = 'Hello ' + my_user.first_name + '! \n' + 'Thanks for registering on Connect Web Application  \n\n  -- Team Connect'
         from_email = settings.EMAIL_HOST_USER
         to_list = [my_user.email]
